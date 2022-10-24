a0481923e0eb0244663efb99c7547326cde98cf8
Richard <richard.sd0007@gmail.com>
2022-10-24 11:05:41 -0700

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,6 +5,25 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
 
+const { restoreUser } = require('../../utils/auth.js');
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
 
 
 
